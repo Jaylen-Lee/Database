de675f2d98e78a49ad826f1a49e14d33bca7fb39
XIAOHAN-LAP <2979397533@qq.com>
2023-12-18 20:27:01 +0800

    order

--- mini_program/pages/orders/orders.js
+++ mini_program/pages/orders/orders.js
@@ -8,23 +8,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    account: "暂无",
     count: 0,
-    ordersList: [],
-    inputdata:'-',
-    hidden:true
+    ordersList: [{
+      "order_number": "000020beb3b359b",
+      "purchase_time": "2021-09-01 02:53:03",
+      "payment_amount": 7319,
+      "payment_method": "alipay",
+      "status": "completed",
+      "user_account": "805762918578633"
+    }],
+    inputdata: '-',
+    hidden: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     var self = this;
+    self.setData({
+      account:wx.getStorageSync('user').account
+    })
     wx.request({
       url: 'http://localhost:8080/orders/findByClient_id',
       data: {
         client_id: wx.getStorageSync('user').id
       },
-      success: function(res) {
+      success: function (res) {
 
         if (res.data.length > 0) {
           self.setData({
@@ -38,18 +49,17 @@ Page({
               [index1]: util.formatTime(new Date(res.data[i].order_time)),
             })
           }
-         
-        }
-        else{
+
+        } else {
           self.setData({
-            hidden:false
-          })          
+            hidden: false
+          })
         }
       }
     })
   },
 
-  bindSelect: function(e) {
+  bindSelect: function (e) {
     wx.navigateTo({
       url: '../ticket/ticket',
     })
@@ -67,49 +77,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     this.onLoad()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
